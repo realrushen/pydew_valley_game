fb7ab7d00a886c861a1832abbf4f80e5ad29ccd4
Karakchiev Aleksandr <realrushen@gmail.com>
2022-08-18 23:46:56 +0300

    added movement to the player

--- src/level.py
+++ src/level.py
@@ -1,16 +1,25 @@
 import pygame
+
+from player import Player
 from settings import *
 
 
 class Level:
     def __init__(self):
+        self.player = None
+
         # get thr display surface
         self.display_surface = pygame.display.get_surface()
 
         # sprite groups
         self.all_sprites = pygame.sprite.Group()
 
+        self.setup()
+
+    def setup(self):
+        self.player = Player((640, 360), group=self.all_sprites)
+
     def run(self, dt):
         self.display_surface.fill('black')
         self.all_sprites.draw(self.display_surface)
-        self.all_sprites.update()
+        self.all_sprites.update(dt)
