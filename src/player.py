3608bd177aca8c09f9771ad62251eed13af22fff
Karakchiev Aleksandr <realrushen@gmail.com>
2022-08-19 23:52:06 +0300

    added player tool animations

--- src/player.py
+++ src/player.py
@@ -2,6 +2,7 @@
 import pygame
 from settings import *
 from support import import_folder
+from timer import Timer
 
 
 class Player(pygame.sprite.Sprite):
@@ -24,6 +25,17 @@ class Player(pygame.sprite.Sprite):
         self.pos = pygame.math.Vector2(self.rect.center)
         self.speed = 200
 
+        # timers
+        self.timers = {
+            'tool use': Timer(350, self.use_tool)
+        }
+
+        # tools
+        self.selected_tool = 'hoe'
+
+    def use_tool(self):
+        print(self.selected_tool)
+
     def import_assets(self):
         self.animations = {'up': [], 'down': [], 'left': [], 'right': [],
                            'right_idle': [], 'left_idle': [], 'up_idle': [], 'down_idle': [],
@@ -36,22 +48,55 @@ class Player(pygame.sprite.Sprite):
             self.animations[animation] = import_folder(full_path)
         print(self.animations)
 
+    def animate(self, dt):
+        self.frame_index += 4 * dt
+        if self.frame_index >= len(self.animations[self.status]):
+            self.frame_index = 0
+        self.image = self.animations[self.status][int(self.frame_index)]
+
     def input(self):
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_UP]:
-            self.direction.y = -1
-        elif keys[pygame.K_DOWN]:
-            self.direction.y = 1
-        else:
-            self.direction.y = 0
+        if not self.timers['tool use'].active:
+            # directions
+            if keys[pygame.K_UP]:
+                self.direction.y = -1
+                self.status = 'up'
+            elif keys[pygame.K_DOWN]:
+                self.direction.y = 1
+                self.status = 'down'
+            else:
+                self.direction.y = 0
 
-        if keys[pygame.K_LEFT]:
-            self.direction.x = -1
-        elif keys[pygame.K_RIGHT]:
-            self.direction.x = 1
-        else:
-            self.direction.x = 0
+            if keys[pygame.K_LEFT]:
+                self.direction.x = -1
+                self.status = 'left'
+            elif keys[pygame.K_RIGHT]:
+                self.direction.x = 1
+                self.status = 'right'
+            else:
+                self.direction.x = 0
+
+            # tool use
+            if keys[pygame.K_SPACE]:
+                self.timers['tool use'].activate()
+                self.direction = pygame.math.Vector2()
+                self.frame_index = 0
+
+    def get_status(self):
+        # idle
+        if self.direction.magnitude() == 0:
+            direction, _, _ = self.status.partition('_')
+            self.status = f'{direction}_idle'
+
+        # tool use
+        if self.timers['tool use'].active:
+            direction, _, _ = self.status.partition('_')
+            self.status = f'{direction}_{self.selected_tool}'
+
+    def update_timers(self):
+        for timer in self.timers.values():
+            timer.update()
 
     def move(self, dt):
         # normalizing direction vector to prevent faster diagonal movement
@@ -68,4 +113,7 @@ class Player(pygame.sprite.Sprite):
 
     def update(self, dt):
         self.input()
+        self.get_status()
+        self.update_timers()
         self.move(dt)
+        self.animate(dt)
