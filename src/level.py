ac089c6c84c4e050e58d3bbc36a5c16af7ddbb58
Karakchiev Aleksandr <realrushen@gmail.com>
2022-08-20 15:48:23 +0300

    implemented player inventory and feature to collect apples and wood in it

--- src/level.py
+++ src/level.py
@@ -47,7 +47,13 @@ class Level:
 
         # trees
         for obj in tmx_data.get_layer_by_name('Trees'):
-            Tree((obj.x, obj.y), obj.image, [self.all_sprites, self.collision_sprites, self.tree_sprites], obj.name)
+            Tree(
+                pos=(obj.x, obj.y),
+                surf=obj.image,
+                groups=[self.all_sprites, self.collision_sprites, self.tree_sprites],
+                name=obj.name,
+                player_add=self.player_add
+            )
 
         # wildflowers
         for obj in tmx_data.get_layer_by_name('Decoration'):
@@ -79,12 +85,17 @@ class Level:
             z=LAYERS['ground']
         )
 
+    def player_add(self, item):
+        self.player.item_inventory[item] += 1
+
     def run(self, dt):
         self.display_surface.fill('black')
         self.all_sprites.custom_draw(self.player)
         self.all_sprites.update(dt)
 
         self.overlay.display()
+
+        print(self.player.item_inventory)
 
 
 class CameraGroup(pygame.sprite.Group):
--- src/sprites.py
+++ src/sprites.py
@@ -64,7 +64,7 @@ class Particle(Generic):
 
 
 class Tree(Generic):
-    def __init__(self, pos, surf, groups, name):
+    def __init__(self, pos, surf, groups, name, player_add):
         super().__init__(pos, surf, groups)
 
         self.health = 5
@@ -77,6 +77,8 @@ class Tree(Generic):
         self.apple_pos = APPLE_POS[name]
         self.apple_sprites = pygame.sprite.Group()
         self.create_fruit()
+
+        self.player_add = player_add
 
     def damage(self):
         # damaging th tree
@@ -91,6 +93,7 @@ class Tree(Generic):
                 groups=self.groups()[0],
                 z=LAYERS['fruit']
             )
+            self.player_add('apple')
             random_apple.kill()
 
     def check_death(self):
@@ -106,6 +109,7 @@ class Tree(Generic):
             self.rect = self.image.get_rect(midbottom=self.rect.midbottom)
             self.hitbox = self.rect.copy().inflate(-10, -self.rect.height * 0.6)
             self.alive = False
+            self.player_add('wood')
 
     def update(self, dt):
         if self.alive:
