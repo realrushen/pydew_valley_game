1e84a0b20bbeb4c5e7918f1dc19b64dc5b2a3c84
Karakchiev Aleksandr <realrushen@gmail.com>
2022-08-20 15:22:15 +0300

    added particle effect for apple removing action

--- src/sprites.py
+++ src/sprites.py
@@ -45,6 +45,23 @@ class WildFlower(Generic):
         self.hitbox = self.rect.copy().inflate(-20, -self.rect.height)
 
 
+class Particle(Generic):
+    def __init__(self, pos, surf, groups, z, duration=200):
+        super().__init__(pos, surf, groups, z)
+        self.start_time = pygame.time.get_ticks()
+        self.duration = duration
+
+        # white surface
+        mask_surf = pygame.mask.from_surface(self.image)
+        new_surf = mask_surf.to_surface()
+        new_surf.set_colorkey((0, 0, 0))
+        self.image = new_surf
+
+    def update(self, dt):
+        current_time = pygame.time.get_ticks()
+        if current_time - self.start_time > self.duration:
+            self.kill()
+
 class Tree(Generic):
     def __init__(self, pos, surf, groups, name):
         super().__init__(pos, surf, groups)
@@ -67,6 +84,12 @@ class Tree(Generic):
         # remove apple
         if len(self.apple_sprites.sprites()) > 0:
             random_apple = random.choice(self.apple_sprites.sprites())
+            Particle(
+                pos=random_apple.rect.topleft,
+                surf=random_apple.image,
+                groups=self.groups()[0],
+                z=LAYERS['fruit']
+            )
             random_apple.kill()
 
     def check_death(self):
